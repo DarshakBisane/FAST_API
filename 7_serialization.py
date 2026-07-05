9c9a9062d3163d9b583ae6767b137bc692749f65
Darshak Bisane <darshak123321@gmail.com>
2026-07-05 21:38:47 +0530

    serialization : means how to export data

---
+++ 7_serialization.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel
+
+class Address(BaseModel):
+    city: str
+    state : str
+    pin : int
+
+class Patient(BaseModel):
+    name : str
+    age : int
+    gender : str
+    address : Address
+
+address1 = {'city':'Nagpur', 'state':'Maharashtra','pin':441907}
+add = Address(**address1)
+
+patient1 = {'name':'Darshak', 'age':19, 'gender': 'Male', 'address' : add}
+patient = Patient(**patient1)
+
+temp = patient.model_dump(exclude=['name']) 
+print(temp)
+
+temp2 = patient.model_dump(include=['name'])
+print(temp2)
+
+temp3 = patient.model_dump(exclude={'address' : 'city'})
+print(temp3)
+
+temp4 = patient.model_dump(include={'address' : 'city'})
+print(temp4)
